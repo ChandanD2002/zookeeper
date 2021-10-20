5d8cdcdf88c95300117cf63eacbff20baa44b7ae
Manjeet <ChandanD@Chandans-MacBook-Air.local>
2021-10-19 23:09:00 -0700

    Finished GET requests

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
 
+app.use(express.static('public'));
 
 
 
@@ -105,6 +106,23 @@ function filterByQuery(query, animalsArray) {
       res.send(404);
     }
   });  
+
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
+
+  app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+  });
+  
+  app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+  });
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
+  
   
   
   app.post('/api/animals', (req, res) => {
